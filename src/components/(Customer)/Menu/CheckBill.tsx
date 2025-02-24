35d5c7df6e489f06ed53196b8480ceca431e163b
Fexx745 <142575597+Fexx745@users.noreply.github.com>
2025-02-24 17:00:23 +0700

    แก้หน้า checkbill

--- src/components/(Customer)/Menu/CheckBill.tsx
+++ src/components/(Customer)/Menu/CheckBill.tsx
@@ -1,9 +1,10 @@
-import { Close, Restaurant, Add, Remove, ShoppingCart, Delete } from '@mui/icons-material';
-import { Skeleton, Dialog, AppBar, Grid, Toolbar, Button, ListItemText, ListItem, Divider, List, Typography, IconButton } from '@mui/material';
+import { Close, Restaurant, Add, Remove, ShoppingCartCheckoutOutlined } from '@mui/icons-material';
+import { Skeleton, Dialog, AppBar, Grid, Toolbar, ListItemText, ListItem, Divider, List } from '@mui/material';
 import { Cart } from "@/types/types";
 import { useCart } from '@/hooks/hooks';
 import { useEffect, useState } from 'react';
 import React from 'react';
+import { API_URL } from "@/utils/config";
 
 interface ShowMenuDetailProps {
     onClose: () => void;
@@ -11,7 +12,7 @@ interface ShowMenuDetailProps {
 
 const CheckBill: React.FC<ShowMenuDetailProps> = ({ onClose }) => {
 
-    const { getCartBy, deleteCartBy } = useCart();
+    const { getCartBy } = useCart();
     const [cartItem, setCartItem] = useState<Cart[]>([]);
     const [loading, setLoading] = useState(true);
 
@@ -23,29 +24,27 @@ const CheckBill: React.FC<ShowMenuDetailProps> = ({ onClose }) => {
         try {
             setLoading(true);
             const res = await getCartBy({ cart_status: 'active' });
+            console.log(res);
+
             setCartItem(res);
         } catch (error) {
             console.error(error);
         } finally {
             setLoading(false);
         }
-    }; 
+    };
 
     return (
         <Dialog fullScreen open={true} onClose={onClose}>
             <AppBar sx={{ position: "relative", backgroundColor: "#f3f4f6", boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)" }}>
-                <Toolbar className="flex justify-between items-center p-4 text-lg font-[600] text-gray-800">
-                    <div className="flex items-center">
-                        <ShoppingCart />
-                        <Typography variant="h6" className="ml-2">ตะกร้า</Typography>
-                    </div>
-                    <IconButton onClick={onClose} className="hover:text-gray-700 text-gray-500">
-                        <Close />
-                    </IconButton>
+                <Toolbar className="flex justify-center p-4 text-lg text-gray-800">
+                    <ShoppingCartCheckoutOutlined /><span className='ml-1 font-[600]'>ตะกร้า</span>
                 </Toolbar>
+                <button className="hover:text-gray-700 text-gray-500 p-2 rounded-full absolute left-2 top-2" onClick={onClose}>
+                    <Close />
+                </button>
             </AppBar>
-
-            <div className="container mx-auto bg-white rounded pb-3 p-6 h-5/6 overflow-auto">
+            <div className="container mx-auto bg-white rounded pb-3 p-6 h-5/6 overflow-auto relative">
                 {loading ? (
                     <Skeleton variant="rectangular" width="100%" height={300} />
                 ) : (
@@ -54,13 +53,14 @@ const CheckBill: React.FC<ShowMenuDetailProps> = ({ onClose }) => {
                             <Grid item xs={12} key={item.cart_id}>
                                 <List className="w-full h-auto">
                                     <ListItem alignItems="flex-start" className="flex justify-between">
-                                        <img
+                                        <img 
                                             className="w-24 h-24 object-cover rounded"
                                         />
                                         <ListItemText
                                             className='ml-5'
                                             primary="asdasd"
-                                            secondary="asdasd" />
+                                            secondary="฿220"
+                                        />
                                         <div className="flex items-center gap-4 rounded-full">
                                             <button
                                                 type="button"
@@ -87,25 +87,20 @@ const CheckBill: React.FC<ShowMenuDetailProps> = ({ onClose }) => {
                         ))}
                     </Grid>
                 )}
-            </div>
-
-            <div className="bg-gray-100 w-full p-4 shadow-xl flex flex-col fixed bottom-16 md:bottom-0 items-center justify-center">
-                <div className="flex justify-between w-full mb-4">
-                    <Typography variant="body1" className="font-medium">รวมทั้งหมด</Typography>
-                    <Typography variant="body1" className="font-medium text-[#36ce75]">฿120</Typography>
+                <div className="absolute">
+                    asd
                 </div>
-                <Button
-                    variant="contained"
-                    color="success"
-                    className="w-full max-w-[320px] rounded-xl py-2 mb-5 font-medium shadow-md hover:shadow-lg transition-all duration-200"
-                    startIcon={<Restaurant />}
-                >
-                    สั่ง 2 รายการ
-                </Button>
             </div>
-
+            <div className="bg-gray-100 w-full p-4 shadow-xl flex flex-col fixed bottom-16 md:bottom-0 items-center justify-center">
+                <div className="flex justify-between w-full">
+                    <span className='text-[18px] font-[500] mb-2'>รวมทั้งหมด  </span>
+                    <span className='text-[18px] font-[500] mb-2'>฿120</span>
+                </div>
+                <button className="bg-[#3fc979] hover:bg-[#36ce75] rounded-xl py-2 mb-5 font-medium text-white text-base shadow-md hover:shadow-lg transition-all duration-200 w-full max-w-[320px]">
+                    <Restaurant className='mr-2' />สั่ง 2 รายการ
+                </button>
+            </div>
         </Dialog>
     );
 };
-
 export default CheckBill;
